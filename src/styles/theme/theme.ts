089ea3ea07b115b9b0dd6c19f5b72e3caddf6609
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2022-04-12 10:47:01 -0300

    refactore: added news elements

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,4 +1,10 @@
 export const theme = {
   colorPrimary:
     'linear-gradient(180deg, rgba(244, 125, 53, 0.66) 0%, rgba(180, 202, 66, 0.65) 100%);',
+
+  colorText: {
+    textGray: '#4f4f4f',
+    textWhite: '#fff',
+  },
+  boxShadow: ' 0px 7px 23px 6px rgba(244, 125, 53, 0.66);',
 };
