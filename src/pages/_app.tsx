5ff6aade6f22d9045f9932bece58a67af7eb9fc8
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2022-04-12 10:46:45 -0300

    feat: added aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@/styles';
 import GlobalStyle from '@/styles/global/globals';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
